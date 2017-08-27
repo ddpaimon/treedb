0591f5461724368cbf35132b64908f720542aaca
RollingShip <ddpaimon@gmail.com>
2017-08-28 01:04:04 +0700

    Realize all backend methods

--- treesite/views.py
+++ treesite/views.py
@@ -11,7 +11,25 @@ from . import serializers
 
 from rest_framework.decorators import api_view
 
+import time
+
 # Create your views here.
+
+testing = False
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
 
 def index(request):
@@ -25,8 +43,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    node = get_object_or_404(Node, pk=question_id)
-    return render(request, 'treesite/detail.html', {'node': node})
+    node_object = get_object_or_404(Node, pk=question_id)
+    return render(request, 'treesite/detail.html', {'node': node_object})
 
 
 def children(request, question_id):
@@ -39,6 +57,11 @@ def children(request, question_id):
 
 
 def free(request):
+    objs = Node.objects
+    all = objs.all()
+    print(all)
+    n1 = objs.get(id=1)
+    print(n1)
     return HttpResponse(request)
 
 
@@ -69,51 +92,82 @@ def subtree(request, node_id):
 
 @api_view(['POST'])
 def update(request):
-    tree_data = request.data
-    read_tree(tree_data['tree'])
-    return JsonResponse(tree_data)
+    tree_data = request.data['tree']
+    db_node_objects = Node.objects
+    updating_tree_data = read_tree(update_or_create_node, tree_data, db_node_objects)
+    db_node_objects = Node.objects
+    resulting_tree_data = read_tree(update_deleted, updating_tree_data, db_node_objects)
+    return JsonResponse({'tree': resulting_tree_data})
 
 
-def read_tree(tree_data):
+def read_tree(func, tree_data, db_node_objects):
     res = list()
     for root_node in tree_data:
-        res.append(read_subtree(root_node))
-
-
-def read_subtree(node_data):
-    children_data = node_data['children']
-    node_data.pop('children', None)
-    res = update_or_create_node(node_data)
-    print(res)
-    res['children'] = list()
-    for child_data in children_data:
-        res['children'].append(read_subtree(child_data))
+        res.append(read_subtree(func, root_node, db_node_objects))
     return res
 
 
-def update_or_create_node(node_data):
-    node_ser = NodeSerializer(data=node_data)
-    if node_ser.is_valid():
-        if node_data['id'] is None:
-            create_node(node_data)
-        else:
-            update_node(node_data)
+def read_subtree(func, node_data, db_node_objects):
+    children_data = node_data['children']
+    node_data.pop('children', None)
+    res = func(node_data, db_node_objects)
+    res['children'] = list()
+    for child_data in children_data:
+        res['children'].append(read_subtree(func, child_data, db_node_objects))
+    # print("Result: ", res)
+    return res
+
+@timeit
+def update_deleted(node_data, db_node_objects):
+    if node_data['id'] is not None:
+        node_object = db_node_objects.get(id=node_data['id'])
+        node_data['deleted'] = node_object.deleted
     return node_data
 
 
-def create_node(node_data):
-    # node_object = Node(**node_data)
-    # parent_node = Node.objects.get(id=node_object.root)
-    # if parent_node.deleted: node_object.deleted = parent_node.deleted
-    # node_object.save()
-    pass
+def update_or_create_node(node_data, db_node_objects):
+    node_ser = NodeSerializer(data=node_data)
+    if node_ser.is_valid():
+        if node_data['id'] is None:
+            node_data = create_node(node_data, db_node_objects)
+        else:
+            node_data = update_node(node_data, db_node_objects)
+    return node_data
 
+@timeit
+def create_node(node_data, db_node_objects):
+    # print("Create node: ", node_data['name'])
+    node_object = Node()
+    node_object.deleted = node_data['deleted']
+    node_object.name = node_data['name']
+    parent_node = db_node_objects.get(id=node_data['root'])
+    node_object.root = parent_node
+    if parent_node.deleted:
+        node_object.deleted = parent_node.deleted
+    if not testing:
+        node_object.save()
+    node_data = NodeSerializer(node_object).data
+    return node_data
 
-def update_node(node_data):
-    # node = Node.objects.get(id=node_data['id'])
-    pass
+@timeit
+def update_node(node_data, db_node_objects):
+    # print("Update node: ", node_data['name'])
+    node_object = db_node_objects.get(id=node_data['id'])
+    if node_object.name != node_data['name']:
+        node_object.name = node_data['name']
+        if not testing:
+            node_object.save()
+    if node_data['deleted'] and node_object.deleted != node_data['deleted']:
+        delete_subtree(node_object, db_node_objects)
+    node_data = NodeSerializer(node_object).data
+    return node_data
 
-
-def delete_node(node_data):
-    # node_object = Node.objects.get(id=node_data['id'])
-    pass
+@timeit
+def delete_subtree(node_object, db_node_objects):
+    node_object.deleted = True
+    if not testing:
+        node_object.save()
+    node_children = db_node_objects.filter(root=node_object.id)
+    if node_children.exists:
+        for child in node_children.iterator():
+            delete_subtree(child, db_node_objects)
